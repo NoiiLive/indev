62ee5c26fad1c7cac3fc0ffd06751b44fd8a04b5
Noii <103219488+NoiiLive@users.noreply.github.com>
2026-03-27 10:39:06 -0700

    Updated ReplicatedStorage/GameData.lua

---
+++ ReplicatedStorage/GameData.lua
@@ -0,0 +1,12 @@
+-- @ScriptType: ModuleScript
+local GameData = {}
+
+GameData.Items = {
+	["Smith & Wesson .38"] = {
+		Name = "Smith & Wesson .38",
+		Type = "Weapon",
+		Description = "A standard issue .38 caliber revolver."
+	}
+}
+
+return GameData
