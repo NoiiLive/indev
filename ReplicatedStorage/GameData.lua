17a27819fdc07e9da2cd825a34788d20297fcadd
Noii <103219488+NoiiLive@users.noreply.github.com>
2026-03-27 12:20:27 -0700

    Updated ReplicatedStorage/GameData.lua

--- ReplicatedStorage/GameData.lua
+++ ReplicatedStorage/GameData.lua
@@ -6,12 +6,20 @@ GameData.Items = {
 		Name = "Smith & Wesson .38",
 		Type = "Weapon",
 		Description = "A standard issue .38 caliber revolver.",
-		Model = "sm38",
+		Model = "sw38",
 		Animations = {
 			Idle = 140360516572974,
 			Walk = 140360516572974,
-			Run = 140360516572974
-		}
+			Run = 140360516572974,
+			Use = {123297753579035},
+			Reload = 109778916509963
+		},
+		MaxClip = 6,
+		FireRate = 0.4,
+		ReloadTime = 1.1,
+		UseSound = "fire_default",
+		ReloadSound = "reload_default"
+		
 	}
 }
 
